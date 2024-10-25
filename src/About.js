6f4b9262e0b2cbecbb5f7b00405d55ebe501d169
art_m <paulric@sheridancollege.ca>
2024-10-24 21:15:30 -0400

    first commit

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are a team dedicated to creating outstanding web experiences.</p>
+    </div>
+  );
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Team from './components/Team';
 
 function App() {
   return (
